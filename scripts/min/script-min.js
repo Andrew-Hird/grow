d44110026817793aaeae29768360200d8d19d1ec
Andrew Hird <andrew.james.hird@gmail.com>
2016-08-22 21:32:50 +1200

    basic functionality working

--- scripts/min/script-min.js
+++ scripts/min/script-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("div").animate({height:200,width:200},3e3),$("div").animate({height:1,width:1},3e3)});
+$(document).ready(function(){setInterval(function(){$("div").animate({height:200,width:200},3e3),$("div").animate({height:0,width:0},3e3)})});
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
-    $("div").animate({
-        height: 200,
-        width: 200,
-    }, 3000)
+        setInterval (function(){
+          $("div").animate({
+              height: 200,
+              width: 200,
+          }, 3000)
 
-    $("div").animate({
-        height: 1,
-        width: 1,
-    }, 3000)
-
+          $("div").animate({
+              height: 0,
+              width: 0,
+          }, 3000)
+        })
 })
