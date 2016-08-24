619f60375218628ed7762040def9895a2ecff7ed
Andrew Hird <andrew.james.hird@gmail.com>
2016-08-24 18:07:16 +1200

    border radius animating

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,40 @@
 $(document).ready(function() {
 
-  //How many boxes?
-  var boxNum = 40
+    //How many boxes?
+    var boxNum = 60
 
-  //Generate Boxes
-  var boxGen = document.getElementById("empty");
-  for (var i = 0; i < boxNum; i++) {
-     boxGen.innerHTML += '<div class="box"></div>';
-  }
+    //Generate Boxes
+    var boxGen = document.getElementById("empty");
+    for (var i = 0; i < boxNum; i++) {
+        boxGen.innerHTML += '<div class="box"></div>';
+    }
 
-  //Change div colour individually
-    $(".box").each(function(){
-  //Random circle colour
-  function randomColor() {
-      r = Math.floor(Math.random() * (256));
-      g = Math.floor(Math.random() * (256));
-      b = Math.floor(Math.random() * (256));
-      jQuery('.box').css('background-color', 'rgb(' + r + ',' + g + ',' + b + ')');
-  }
-      randomColor();
-      var t = setInterval(randomColor, 500);
+    //Change div colour individually
+    //Random circle colour
+    function randomColor() {
+        r = Math.floor(Math.random() * (256))
+        g = Math.floor(Math.random() * (256))
+        b = Math.floor(Math.random() * (256))
+        $('.box').each(function() {
+            $(this).css('background-color', 'rgb(' + r + ',' + g + ',' + b + ')')
+        })
+    }
+    randomColor();
+    var t = setInterval(randomColor, 500)
+
+
+    //Circle grow and shrink loop
+    setInterval(function() {
+        $(".box").animate({
+            height: 500,
+            width: 500,
+            'border-radius': '50%'
+        }, 10000)
+
+        $(".box").animate({
+            height: 0,
+            width: 0,
+            'border-radius': '0%'
+        }, 10000)
     })
-
-  //Circle grow and shrink loop
-  setInterval(function() {
-      $(".box").animate({
-          height: 1000,
-          width: 1000,
-      }, 10000)
-
-      $(".box").animate({
-          height: 0,
-          width: 0,
-      }, 3000)
-  })
 })
